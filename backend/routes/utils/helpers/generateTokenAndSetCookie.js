0b602bd0a4ff15c1fa37766b74f397dce17b8162
teejay009 <tajdjimoh@gmail.com>
2024-02-24 15:37:28 +0100

    update

--- backend/routes/utils/helpers/generateTokenAndSetCookie.js
+++ backend/routes/utils/helpers/generateTokenAndSetCookie.js
@@ -1,4 +1,14 @@
+import { Jwt } from "jsonwebtoken";
+
 const generateTokenAndSetCookies = (userId,res) => {
+    const token = jwt.sign({userId},process.env.JWT_SECRET {
+        expiresIn: '15d',
+    })
+
+    res.cookies("jwt", token, {
+        httpOnly: true, // This cookies can not be accessed by the browser. more secure
+        maxAge: 15
+    })
 
 }
 
