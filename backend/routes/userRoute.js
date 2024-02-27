255911207972527713e6a72d234156af4815db6a
teejay009 <tajdjimoh@gmail.com>
2024-02-27 12:51:33 +0100

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signupUser } from '../controllers/userController.js';
+import { losignupUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
