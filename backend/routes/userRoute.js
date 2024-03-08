a498201445e78f90b2c2a11637034a2328842f85
teejay009 <tajdjimoh@gmail.com>
2024-03-08 13:21:18 +0100

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/signup', signupUser); 
 router.post('/login', loginUser); 
 router.post('/logout', logoutUser); 
-router.post("/follow/:id", pro followUnfollowUser);
+router.post("/follow/:id", prot followUnfollowUser);
 
 
 
