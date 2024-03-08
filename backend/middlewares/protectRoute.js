d0fe2166cb483b52638ce7c33c8a03bb7e8478d5
teejay009 <tajdjimoh@gmail.com>
2024-03-08 13:37:22 +0100

    update

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -2,6 +2,9 @@ const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
 
+        if(!token) return res.status(401).json({ message: "Unauthorized" });
+        const decoded = jwt.verify(token
+
     } catch (error) {
         res.status(500).json({ message: err.message});
         console.log("Error in signupUser: ", err.message);
