3f679b3ebf08f7d6e6898dd98c30733b77b8becf
teejay009 <tajdjimoh@gmail.com>
2024-03-08 16:04:38 +0100

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { signupUser, loginUser, logoutUser, followUnfollowUser } from '../controllers/userController.js';
+import protectRoute from '../middlewares/protectRoute.js';
 
 const router = express.Router();
 
