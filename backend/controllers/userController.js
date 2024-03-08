b974cee3c35b3e2e6ff1edcfa6f0a2670e47f770
teejay009 <tajdjimoh@gmail.com>
2024-03-08 15:16:36 +0100

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -87,6 +87,7 @@ const followUnfollowUser = async (req, res) => {
         const userTomodify = await User.findById(id);
         const currentUser = await User.findById(req.user._id);
         if(id === req.user._id) return res.status(400).json({ message: "You cannot follow/unfollow your self" });
+        if(!userTomodify)
 
     } catch (err) {
         res.status(500).json({ message: err.message });
