76d98c7bb025073603421e23745de6f87c2f3131
teejay009 <tajdjimoh@gmail.com>
2024-03-09 12:44:27 +0100

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,6 +96,7 @@ const followUnfollowUser = async (req, res) => {
             // Modify current User following, modify followers of userToModify
             await User.findByIdAndUpdate(req.user._id, { $pull: { following: id} });
             await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+            res.status
         } else {
             // follow user
             await User.findByIdAndUpdate(id, { $push: { followers: req.user._id} });
