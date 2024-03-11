8f029474b16371c8df5f714ef4ad3d9f53a47ca4
teejay009 <tajdjimoh@gmail.com>
2024-03-11 16:20:16 +0100

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -113,6 +113,13 @@ const followUnfollowUser = async (req, res) => {
     }
 };
 
-const updateUser = (req, res) => {}
+const updateUser = (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log("Error in updateUser: ", err.message);
+}
+};
 
 export { signupUser, loginUser, logoutUser, followUnfollowUser };
