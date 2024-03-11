e04891d24fae23bd2cab24980459393285d61d33
teejay009 <tajdjimoh@gmail.com>
2024-03-11 16:00:01 +0100

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -101,7 +101,7 @@ const followUnfollowUser = async (req, res) => {
             // follow user
             await User.findByIdAndUpdate(id, { $push: { followers: req.user._id} });
             await User.findByIdAndUpdate(req.user._id, { $push: { following: id} });
-            res.status(200).json({ message: " User followed successfull "});
+            res.status(200).json({ message: " User followed successfully "});
 
             
         }
