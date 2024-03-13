6120b2262a5a08a7bf34abf803a7d388b7f4e5df
teejay009 <tajdjimoh@gmail.com>
2024-03-13 10:21:02 +0100

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -118,7 +118,14 @@ const updateUser = async (req, res) => {
     const userId = req.user._id;
     try {
         let user = await User.findById(userId);
-        if (!user) {
+        if (!user) return res.status(400).json({ message; "User not found" });
+
+        if (password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            User.password = hashedPassword;
+        }
+        user.name = name || username.name
 
     } catch (err) {
         res.status(500).json({ message: err.message });
