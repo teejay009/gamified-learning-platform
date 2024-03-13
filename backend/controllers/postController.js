041eab9515290e8acfd8b26702fc8a03286566a3
teejay009 <tajdjimoh@gmail.com>
2024-03-13 14:45:12 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -9,6 +9,13 @@ const createPost = async (req, res) => {
         }
 
         const user = await User.findById(postedBy);
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+
+        if(user._id.toString() !== req.user._id.toString()){
+            return res.status(401).json({message: "Unauthorized to create post"})
+        }
 
     } catch (err) {
         res.status(500).json({message: err.message });
