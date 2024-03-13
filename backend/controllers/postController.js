5124e4d0d04678fc9b16091281bb514429a07611
teejay009 <tajdjimoh@gmail.com>
2024-03-13 14:24:20 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -6,7 +6,8 @@ const createPost = async (req, res) => {
             return res.status(400).json({message: "please fill all the fields"})
         }
     } catch (err) {
-        res.status(500).json({message: })
+        res.status(500).json({message: err.message });
+        console.log(err)
     }
 }
 
