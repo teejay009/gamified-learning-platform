c409dcdf0eb140c0fa2aecc225107e8c9af38cee
teejay009 <tajdjimoh@gmail.com>
2024-03-13 14:33:15 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -8,7 +8,8 @@ const createPost = async (req, res) => {
             return res.status(400).json({message: "postedby and text fields are required"})
         }
 
-        const user
+        const user = await User.findById(postedBy);
+        
     } catch (err) {
         res.status(500).json({message: err.message });
         console.log(err)
