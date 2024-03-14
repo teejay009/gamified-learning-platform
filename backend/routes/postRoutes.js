7776f5bd43bbfaf92e74544d419a5b702e46005c
teejay009 <tajdjimoh@gmail.com>
2024-03-14 14:55:19 +0100

    update

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
 const app = express();
-import { createPost, getPost } from "../controllers/postController.js";
+import { createPost, deletePost, getPost } from "../controllers/postController.js";
 import protectRoute from '../middlewares/protectRoute.js';
 
 
 const router = express.Router();
 
-router.get("/",getPost)
-router.post("/create", protectRoute, createPost)
+router.get("/id",getPost);
+router.post("/create", protectRoute, createPost);
 router.delete("/:id", deletePost);
 
 export default router;
