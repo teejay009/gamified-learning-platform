3c71e68e56f9d039e6e387273f97543f63925e91
teejay009 <tajdjimoh@gmail.com>
2024-03-14 14:44:25 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -57,6 +57,9 @@ const deletePost = async (req, res) => {
         if (!post) {
             return res.status(404).json({ message: "No post found" });
         }
+        if (post.posteedBy.toString() === req.user._id.toString()) {
+            return res.status(401).json({ message: "unauthorized to delete post" });
+        }
 
     }catch (err) {
         res.status(500).json({message: err.message});
