8d9cb304384816c46dcc7e28b676445f6e7c85a2
teejay009 <tajdjimoh@gmail.com>
2024-03-14 16:01:12 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -95,5 +95,14 @@ res.status(200).json({message: "Post unliked successfully"});
     }
 };
 
-const reply
+const replyToPost = async (req, res) => {
+    try {
+        const {text} = req.body;
+        const {id} = req.params;
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
+
 export { createPost, getPost, deletePost, likeUnlikePost, replyToPost };
