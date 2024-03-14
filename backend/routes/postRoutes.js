bcb447ba8b14e6253fe411f4fda49f1f58da4275
teejay009 <tajdjimoh@gmail.com>
2024-03-14 15:40:17 +0100

    update

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -11,6 +11,7 @@ router.get("/id",getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id",protectRoute, deletePost);
 router.post("/like:id",protectRoute, likeUnlikedPost);
+router.post("/like:id",protectRoute, likeUnlikedPost);
 
 export default router;
 
