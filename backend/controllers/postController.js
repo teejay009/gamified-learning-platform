f35054c3980e6f59380820458bfcd1d8ffefd5a8
teejay009 <tajdjimoh@gmail.com>
2024-03-14 14:36:13 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -54,6 +54,8 @@ const getPost = async (req, res) => {
 const deletePost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ message: "No post found" })
 
     }catch (err) {
         res.status(500).json({message: err.message});
