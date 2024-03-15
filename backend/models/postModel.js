8ea1c35bd8d10aef3c5bbab7a51ce4b7641b93d4
teejay009 <tajdjimoh@gmail.com>
2024-03-15 12:51:20 +0100

    update

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -37,7 +37,7 @@ const postSchema = mongoose.Schema({
             },
             username:{
                 type:true,
-                required: true
+                
 
             }
         }
