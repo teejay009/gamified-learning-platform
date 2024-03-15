f4d032e4afae07dfa6c073f769651d45ca38440d
teejay009 <tajdjimoh@gmail.com>
2024-03-15 16:26:00 +0100

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -124,6 +124,8 @@ const getFeedPosts = async (req, res) => {
         if(!user){ 
             return res.status(404).json({ message: "User not found" })
         }
+        const following = user.following;
+        
 
     } catch (err) {
         res.status(500).json({ message: err.message });
